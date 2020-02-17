50ceaf6b77fe96930aac64a65a689f890d422e28
Ishan Singhania <ishans@datetheramp.com>
2020-02-17 17:19:55 +0530

    #8-pagination: updated bank pipe to filter on all the keys and not just the bank name.

--- src/app/utils/bank-search.pipe.ts
+++ src/app/utils/bank-search.pipe.ts
@@ -1,16 +1,21 @@
+import * as _ from 'lodash';
 import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
 	name: 'bankSearchPipe',
 })
 export class BankSearchPipe implements PipeTransform {
-
 	transform(items: any[], searchText: string): any[] {
 		if (!items) return [];
 		if (!searchText) return [...items];
 		searchText = searchText.toLowerCase();
 
-		return items.filter(it => {
-			return it.bank_name.toLowerCase().includes(searchText);
+		return _.filter(items, function(item) {
+			const columnKeys = _.keys(item);
+			return _.find(columnKeys, function(key) {
+				if (item[key]) {
+					return _.includes(_.lowerCase(item[key]), searchText);
+				}
+			});
 		});
 	}
 }
